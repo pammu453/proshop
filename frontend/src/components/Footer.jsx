fb35fb7abec0f8f4cbfe53f095694f2544682617
pammu453 <111453798+pammu453@users.noreply.github.com>
2023-11-27 20:09:52 +0530

    Added header and footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { Container,Row,Col } from "react-bootstrap"
+
+const Footer = () => {
+    const currentYear =new Date().getFullYear()
+  return (
+    <footer>
+      <Container>
+        <Row>
+            <Col >
+            <p className="text-center my-0">Pro-Shop &copy; {currentYear}</p>
+            <p className="text-center">Made with &#10084; &#65039; by Pramod Savant</p>
+            </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
