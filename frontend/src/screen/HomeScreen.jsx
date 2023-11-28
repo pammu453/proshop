e85fa17bc530bc3c0ab6ce8d520e140210667af0
pammu453 <111453798+pammu453@users.noreply.github.com>
2023-11-28 22:46:53 +0530

    Added proxy at frontend

--- frontend/src/screen/HomeScreen.jsx
+++ frontend/src/screen/HomeScreen.jsx
@@ -8,7 +8,7 @@ const HomeScreen = () => {
 
     useEffect(()=>{
        const fetchProducts=async()=>{
-         const {data}=await axios.get("http://localhost:5000/api/products")
+         const {data}=await axios.get("/api/products")
          setProducts(data)
        }
        fetchProducts()
