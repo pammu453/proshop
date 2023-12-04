3004eb1290a359faba80aa63a9990d12927dc241
pammu453 <111453798+pammu453@users.noreply.github.com>
2023-12-04 19:14:16 +0530

    prepared sample data

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs'
+
+const users=[
+    {
+        name:"Admin User",
+        email:"admin@gmail.com",
+        password:bcrypt.hashSync("1234",10),
+        isAdmin:true
+    },
+    {
+        name:"Pramod Savant",
+        email:"pramodsavant@gmail.com",
+        password:bcrypt.hashSync("1234",10),
+        isAdmin:false
+    },
+    {
+        name:"Amar KS",
+        email:"amarks@gmail.com",
+        password:bcrypt.hashSync("1234",10),
+        isAdmin:false
+    },
+]
+
+export default users
