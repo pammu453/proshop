6f2f2141cbd9ba17d9c01f669229314243c5729b
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-01-05 22:18:03 +0530

    sameSite: "none"

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,8 +6,8 @@ const generateToken = (res, userId) => {
     //Set JWT as HTTP_Only cookie
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: "Strict",
+        secure: true,
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000,//30 days
     })
 }
