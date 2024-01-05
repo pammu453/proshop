5741e73bc2b8d18b73504797e3748afe7139fde6
pammu453 <111453798+pammu453@users.noreply.github.com>
2024-01-05 21:50:52 +0530

    tiken error fixed

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId) => {
     //Set JWT as HTTP_Only cookie
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV === 'production',
         sameSite: "Strict",
         maxAge: 30 * 24 * 60 * 60 * 1000,//30 days
     })
